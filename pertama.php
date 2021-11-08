479b6bbcb6cfa4116af4e85be7489bf117ed0e4d
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2021-11-08 12:09:28 +0700

    up file index

--- pertama.php
+++ pertama.php
@@ -1,11 +1,44 @@
 <?php
 
-echo "<h2> Ini adalah kata pertama </h2>";
+echo "Mari belajar PHP<br>";
+echo "Bersama Mas Bayu<br>";
+
 $a=8;
-$b=2;
+$b=4;
+$f=10;
+$g=6;
+echo "Nilai dari variabel a adalah ".$a."<br>";
+echo "Nilai dari variabel b adalah ".$b."<br>";
+$c=$a+$b;
+$d=$a*$b;
+$e=$a/$b;
+echo "Nilai dari ".$a."+".$b."=".$c."<br>";
+echo "Nilai dari ".$a."x".$b."=".$d."<br>";
+echo "Nilai dari ".$a.":".$b."=".$e."<br>";
 
-echo "Nilai dari variabel a adalah :".$a."<br>";
+$nilai=4;
+if ($nilai>=7) {
+	echo "Nilai Sempurna";
+} elseif($nilai>=5) {
+	echo "Nilai Cukup";
+} else {
+	echo "Nilai Kurang";
+}
+echo "<br>";
 
-echo "Nilai dari variabel b adalah :".$b."<br>";
+function kurangi($w,$s){
+	$subs=$w-$s;
+	echo "Nilai dari ".$w."-".$s."=".$subs."<br>";
+}
+function modulus($p,$t){
+	$mod=$p%$t;
+	echo "Nilai dari ".$p."%".$t."=".$mod."<br>";
+}
 
+kurangi($a,$b);
+kurangi($f,$g);
+kurangi($g,$b);
+kurangi($b,$f);
+modulus($a,$b);
+modulus($g,$b);
 ?>
